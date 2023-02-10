a4b600b1f7db51aaf04bca683440c2d27dac8429
zhaoxiaofei <xiaofei.zhao@bluefocus.com>
2023-02-10 19:27:25 +0800

    feat: rocketmq borker with logger

--- broker/rocketmq/logger.go
+++ broker/rocketmq/logger.go
@@ -7,23 +7,23 @@ type logger struct {
 }
 
 func (l logger) Debug(msg string, fields map[string]interface{}) {
-	l.Logger.Log(log.LevelDebug, msg)
+	l.Logger.Log(log.LevelDebug, "msg", msg)
 }
 
 func (l logger) Info(msg string, fields map[string]interface{}) {
-	l.Logger.Log(log.LevelInfo, msg)
+	l.Logger.Log(log.LevelInfo, "msg", msg)
 }
 
 func (l logger) Warning(msg string, fields map[string]interface{}) {
-	l.Logger.Log(log.LevelWarn, msg)
+	l.Logger.Log(log.LevelWarn, "msg", msg)
 }
 
 func (l logger) Error(msg string, fields map[string]interface{}) {
-	l.Logger.Log(log.LevelError, msg)
+	l.Logger.Log(log.LevelError, "msg", msg)
 }
 
 func (l logger) Fatal(msg string, fields map[string]interface{}) {
-	l.Logger.Log(log.LevelFatal, msg)
+	l.Logger.Log(log.LevelFatal, "msg", msg)
 }
 
 func (l logger) Level(level string) {
